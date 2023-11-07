ac3349c62dcbd8f3d65c29e90275cc629880139a
lizzlyna <barbara.panzetta@gmail.com>
2023-11-07 16:56:46 +0100

    local storage

--- m2/s7/g1/esercizio.pom.js
+++ m2/s7/g1/esercizio.pom.js
@@ -1,4 +1,4 @@
-class User {
+{class User {
    constructor (f,s,a,l){
     this.firstName = f;
     this.surname = s;
@@ -19,5 +19,22 @@ let Barbara=new User ('Barbara', 'Panzetta', 47, 'Casole')
 
 let confronto = Clara.ageConfronter (Barbara);
 console.log(confronto);
+}
 
+
+{class Pet {
+    constructor (pn, on, s, b) {
+        this.petname = pn;
+        this.ownername= on;
+        this.species = s;
+        this.breed=b;
+    }
+}}
+
+ownerConfronter 
+
+let Temple=new Pet ('Temple', 'Clara', 'gatto', 'Pugliese')
+let Beato=new Pet ('Beato', 'Pino', 'gatto', 'Pugliese')
+let Batto=new Pet ('Batto', 'Ciro', 'batto', 'leopard')
+let Cassio=new Pet ('Cassio', 'Edoardo', 'gatto', 'Pugliese')
 
