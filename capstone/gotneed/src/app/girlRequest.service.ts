4e4b8a235c9609ffd284297f0f426e07f0d766ac
lizzlyna <barbara.panzetta@gmail.com>
2024-03-14 20:19:11 +0100

    girl request

---
+++ capstone/gotneed/src/app/girlRequest.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GirlRequest } from './models/girlRequest.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GirlRequestService {
+  private baseUrl = 'http://localhost:8080'; 
+  constructor(private http: HttpClient) { }
+
+  createGirlRequest(girlRequest: GirlRequest): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/register`, girlRequest);
+  }
+}
---
+++ capstone/gotneed/src/app/models/girlRequest.model.ts
@@ -0,0 +1,8 @@
+export interface GirlRequest {
+    nome: string;
+    cognome: string;
+    username: string;
+    email: string;
+    dataNascita: Date;
+    avatar: string;
+}
