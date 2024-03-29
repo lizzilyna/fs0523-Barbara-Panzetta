fef818a81de81382f1806f6d2941d1a03580d481
lizzlyna <barbara.panzetta@gmail.com>
2024-03-29 19:24:34 +0100

    form enum

--- capstone/gotneed/src/app/app-routing.module.ts
+++ capstone/gotneed/src/app/app-routing.module.ts
@@ -4,12 +4,21 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { GirlFormComponent } from './girl-form/girl-form.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GotComponent } from './got/got.component';
+import { NeedComponent } from './need/need.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent},
   { path: 'girl-form', component: GirlFormComponent },
+  { path: 'dashboard', component: DashboardComponent, children: [
+    {path: 'got', component: GotComponent},
+    {path: 'need', component: NeedComponent},
+    {path: 'search', component: SearchComponent},
+  ]}
 
 ];
 
